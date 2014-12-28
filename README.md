1e25838486d25356c5124b0827247fb1d3126555
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-12-28 22:38:30 +0200

    readme

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
 # ngKookies
 
+> 
+
+## Install
+
+You can download `ngKookies.js` manually or install it via [bower]():
+
+```bash
+bower install ngKookies
+```
+
+or via [npm]()
+
+```bash
+npm install ngKookies
+```
+
+## Usage
+
+### Create cookies
+
+```javascript
+var app = angular.module('exampleApp', ['ngKookies']);
+
+app.controller('MainCtrl', function ($kookies) {
+    // 1. create session cookie
+    $kookies.set('name', 'value');
+
+    // 2. create expiring cookie
+    $kookies.set('name', 'value', {expires: 7});
+
+    // 3. Create expiring cookie, valid across entire site
+    $kookies.set('name', 'value', {expires: 7, path: '/'});
+});
+```
+
+### Read cookies
+
+```javascript
+var app = angular.module('exampleApp', ['ngKookies']);
+
+app.controller('MainCtrl', function ($kookies) {
+    $kookies.get('name'); // "value"
+    $kookies.get('nothing'); // undefined
+
+    // Read all available cookies
+    $kookies.get();
+});
+```
+
+### Delete cookie
+
+```javascript
+var app = angular.module('exampleApp', ['ngKookies']);
+
+app.controller('MainCtrl', function ($kookies) {
+    $kookies.remove('name'); // true
+    $kookies.remove('nothing'); // false
+
+    // Read all available cookies
+    $kookies.get();
+});
+```
+
+It's necessary pass the exact same path, domain and secure options that were used to set the cookie, unless you're relying on the default options:
+
+```javascript
+var app = angular.module('exampleApp', ['ngKookies']);
+
+app.controller('MainCtrl', function ($kookies) {
+    $kookies.set('name', 'value', {path: '/'});
+
+    $kookies.remove('name'); // false
+    // use the same options (path, domain) as what the cookie was written with
+    $kookies.remove('name', {path: '/'}); // true
+});
+```
+
+## References
--- ngKookies.js
+++ ngKookies.js
@@ -19,8 +19,65 @@
     angular.module('ngKookies', [])
 
     .provider('ngKookies', function () {
+        var defaults = {};
+
+        this.setDefaults = function (newDefaults) {
+            angular.extend(defaults, newDefaults);
+        };
+
         this.$get = [function () {
 
+            var privateMethods = {};
+            var publicMethods = {};
+
+            publicMethods.get = function (key, converter) {
+                var result = key ? void 0 : {};
+                var cookies = document.cookie ? document.cookie.split('; ') : [];
+
+                for (var i = 0, l = cookies.length; i < l; i++) {
+                    var parts = cookies[i].split('=');
+                    var name = privateMethods.decode(parts.shift());
+                    var cookie = parts.join('=');
+
+                    if (key && key === name) {
+                    // If second argument (value) is a function it's a converter...
+                result = read(cookie, value);
+                break;
+            }
+
+            // Prevent storing a cookie that we couldn't decode.
+            if (!key && (cookie = read(cookie)) !== undefined) {
+                result[name] = cookie;
+            }
+        }
+
+        return result;
+            };
+
+            publicMethods.set = function (key, value, options) {
+                options = angular.extend(options, defaults);
+
+                if (typeof options.expires === 'number') {
+                    var days = options.expires;
+                    var time = options.expires = new Date();
+                    time.setTime(+time + days * 864e+5);
+                }
+
+                return (document.cookie = [
+                    encode(key), '=', stringifyCookieValue(value),
+                    // use expires attribute, max-age is not supported by IE
+                    options.expires ? '; expires=' + options.expires.toUTCString() : '',
+                    options.path    ? '; path=' + options.path : '',
+                    options.domain  ? '; domain=' + options.domain : '',
+                    options.secure  ? '; secure' : ''
+                ].join(''));
+            };
+
+            publicMethods.remove = function (key) {
+
+            };
+
+            return publicMethods;
         }];
     });
 }));
